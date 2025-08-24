9b002513dbba63942d80e631a0bd55d736abca13
Kartik Sharma <poojan4604@gmail.com>
2025-08-24 15:30:06 +0530

    saturn added

--- src/components/Canvas.jsx
+++ src/components/Canvas.jsx
@@ -2,14 +2,17 @@ import {Canvas} from '@react-three/fiber';
 import {OrbitControls} from '@react-three/drei';
 import Neptune from './Neptune';
 import Earth from './earth';
-
+import Saturn from './Saturn';
+import Mars from './mars';
 
 export default function Canvas_3(){
     return(
         <div className="w-screen h-screen bg-black">
             
             <Canvas camera={{ position: [0, 0, 30] }}>
-                <Earth/>
+                
+            <Mars/>
+                
                {/*<Neptune/>*/}
                 <ambientLight intensity={0.8}/>
                 <directionalLight position={[5,5,5]} intensity={1}/>
